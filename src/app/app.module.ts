72f4b3489ee6ce36b0112bf1abf4a4e5e7a56a7a
ThanhChau82 <thanh.dalencon@gmail.com>
2021-06-13 01:38:51 +0200

    Sprint 0 : Créer l'arborescence du projet : ajouter les composants (list, edit, add, modify), 1 service et 1 classe model pour la gestion des produits chimiques

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { EditComponent } from './edit/edit.component';
+import { AddComponent } from './add/add.component';
+import { ModifyComponent } from './modify/modify.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    EditComponent,
+    AddComponent,
+    ModifyComponent
   ],
   imports: [
     BrowserModule,
