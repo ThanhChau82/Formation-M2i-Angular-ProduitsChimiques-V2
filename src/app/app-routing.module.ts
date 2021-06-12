338bbbfbbbdb5111b5c6a4377df0f1755959c2f3
ThanhChau82 <thanh.dalencon@gmail.com>
2021-06-13 01:53:53 +0200

    Sprint 1 : Paramétrer la navigation entres les composants

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
+import { ListComponent } from './list/list.component';
+import { ModifyComponent } from './modify/modify.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'list', component : ListComponent},
+  {path : 'edit', component : EditComponent},
+  {path : 'modify', component : ModifyComponent},
+  {path : 'add', component : AddComponent},
+  {path : '', redirectTo : 'list', pathMatch : 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
