83987d35b69b600668377f58097a26ac1f4d1271
ThanhChau82 <thanh.dalencon@gmail.com>
2021-06-13 10:34:20 +0200

    Sprint 1 : ajouter le model Produit et le ProduitService scope global

--- src/app/models/produit.ts
+++ src/app/models/produit.ts
@@ -1,2 +1,22 @@
 export class Produit {
+  id : number;
+  nom : string;
+  cas : string;
+  formule : string;
+  masseMoleculaire : number;
+  description : string;
+  quantite : number;
+  localisation : string;
+
+  constructor(id : number, nom : string, cas : string, formule : string, masseMoleculaire : number, description : string, quantite : number, localisation : string) {
+    this.id = id;
+    this.nom = nom;
+    this.cas = cas;
+    this.formule = formule;
+    this.masseMoleculaire = masseMoleculaire;
+    this.description = description;
+    this.quantite = quantite;
+    this.localisation = localisation;
+  }
 }
+
--- src/app/services/produit-service.service.ts
+++ src/app/services/produit-service.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Produit } from '../models/produit';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProduitServiceService {
+  produits : Produit[];
 
-  constructor() { }
+  constructor() {
+    this.produits = [
+      new Produit(1, "n-Butyllithium solution", "109-72-8", "CH3(CH2)3Li", 64.06, "1.6M in hexanes", 3, "LSO-S305"),
+      new Produit(2, "Methanol", "67-56-1", "CH3OH", 32.04, "Methanol, anhydrous, 99.8%", 5, "LSO-S101")
+    ];
+  }
+
+  getAll() : Produit[] {
+    return [...this.produits]; // Retourner une copie de valeur de tableau de produits.
+  }
+
+  getById(id : number) : Produit {
+    return <Produit>{...this.produits.find(p => p.id == id)};
+  }
+
+  add(produit : Produit) : void {
+    this.produits.push(produit);
+  }
+
+  modify(produit : Produit) : void {
+    let indice = this.produits.findIndex(p => p.id == produit.id);
+    this.produits[indice] = produit;
+  }
+
+  delete(id : number) : void {
+    this.produits = this.produits.filter(p => p.id != id);
+  }
+
 }
