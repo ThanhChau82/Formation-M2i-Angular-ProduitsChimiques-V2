914799dafb9a6fc51c23e03ece9d8b2610948bb6
ThanhChau82 <thanh.dalencon@gmail.com>
2021-06-13 11:02:10 +0200

    Sprint 3 - Ajouter un produit

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Produit } from '../models/produit';
+import { ProduitService } from '../services/produit-service.service';
 
 @Component({
   selector: 'app-add',
@@ -7,9 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddComponent implements OnInit {
 
-  constructor() { }
+  constructor(private produitService : ProduitService, private router : Router) { }
 
   ngOnInit(): void {
   }
 
+  ajouter(f : NgForm) {
+    let produit = new Produit(f.value.id, f.value.nom, f.value.cas, f.value.formule, f.value.masse, f.value.description, f.value.quantite, f.value.localisation);
+    this.produitService.add(produit);
+    this.router.navigate(["/list"]);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ListComponent } from './list/list.component';
 import { EditComponent } from './edit/edit.component';
 import { AddComponent } from './add/add.component';
 import { ModifyComponent } from './modify/modify.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ModifyComponent } from './modify/modify.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
